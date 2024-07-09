e03374412480b92f038f505880b273c93ed6a77d
Yan Cao <ycao77@stanford.edu>
2024-07-09 14:29:26 -0700

    Fix the bug of requirement validation

--- radx-metadata-validator-lib/src/main/java/edu/stanford/bmir/radx/metadata/validator/lib/Validator.java
+++ radx-metadata-validator-lib/src/main/java/edu/stanford/bmir/radx/metadata/validator/lib/Validator.java
@@ -53,7 +53,8 @@ public class Validator {
       var instanceNode = JsonLoader.loadJson(instanceContent, "Instance");
 
       //validate the template is CEDAR model template
-      cedarSchemaValidatorComponent.validate(templateNode, consumer);
+      //TODO: patch template
+//      cedarSchemaValidatorComponent.validate(templateNode, consumer);
 
       if(passValidation(results)){
         //Read template and get valueConstraints map
@@ -73,7 +74,7 @@ public class Validator {
 
         if(passValidation(results)){
           //validate required fields
-          requiredFieldValidatorComponent.validate(templateReporter, templateInstanceValuesReporter, consumer);
+          requiredFieldValidatorComponent.validate(templateSchemaArtifact, templateReporter, templateInstanceValuesReporter, consumer);
 
           //validate data type
           dataTypeValidatorComponent.validate(templateReporter, templateInstanceValuesReporter, consumer);
