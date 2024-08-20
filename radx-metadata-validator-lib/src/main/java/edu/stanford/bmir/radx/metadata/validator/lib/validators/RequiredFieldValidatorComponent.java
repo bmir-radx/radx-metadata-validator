face2102b4a48db67e998e67de33844401594b38
Yan Cao <ycao77@stanford.edu>
2024-08-20 14:36:52 -0700

    clean RequiredFieldValidatorComponent

--- radx-metadata-validator-lib/src/main/java/edu/stanford/bmir/radx/metadata/validator/lib/validators/RequiredFieldValidatorComponent.java
+++ radx-metadata-validator-lib/src/main/java/edu/stanford/bmir/radx/metadata/validator/lib/validators/RequiredFieldValidatorComponent.java
@@ -13,45 +13,6 @@ import java.util.function.Consumer;
 public class RequiredFieldValidatorComponent {
   private final FieldsCollector fieldsCollector = new FieldsCollector();
 
-//  public void validate(TemplateReporter templateReporter, TemplateInstanceValuesReporter valuesReporter, Consumer<ValidationResult> handler){
-//    var values = valuesReporter.getValues();
-//    //Iterate the valuesReporter
-//    for (Map.Entry<String, FieldValues> fieldEntry : values.entrySet()) {
-//      String path = fieldEntry.getKey();
-//      FieldValues fieldValues = fieldEntry.getValue();
-//      var jsonLdValue = fieldValues.jsonLdValue();
-//      var jsonLdId = fieldValues.jsonLdId();
-//      var jsonLdLabel = fieldValues.label();
-//      var valueConstraint = templateReporter.getValueConstraints(path);
-//      //If it is required
-//      if(valueConstraint.isPresent()){
-//        if(valueConstraint.get().requiredValue()){
-//          String errorMessage = "Missing required value at " + path;
-//          // If it is link type, check @id
-//          if (valueConstraint.get().isLinkValueConstraint()){
-//            if(jsonLdId.isEmpty() || jsonLdId.get().toString().equals("")){
-//              handler.accept(new ValidationResult(ValidationLevel.ERROR, ValidationName.REQUIREMENT_VALIDATION, errorMessage, path));
-//            }
-//          } else if (valueConstraint.get().isControlledTermValueConstraint()){//if it is controlled term, check @label and @id
-//            if (jsonLdLabel.isEmpty() || jsonLdLabel.get().equals("")){
-//              var errorMessage2 = "rdfs:label is missing" + " at " + path;
-//              handler.accept(new ValidationResult(ValidationLevel.ERROR, ValidationName.REQUIREMENT_VALIDATION, errorMessage2, path));
-//            }
-//            if (jsonLdId.isEmpty() || jsonLdId.get().toString().equals("")){
-//              var errorMessage3 = "@id is missing" + " at " + path;
-//              handler.accept(new ValidationResult(ValidationLevel.ERROR, ValidationName.REQUIREMENT_VALIDATION, errorMessage3, path));
-//            }
-//          } else {
-//            //For others, check @value
-//            if (jsonLdValue.isEmpty() || jsonLdValue.get().equals("")){
-//              handler.accept(new ValidationResult(ValidationLevel.ERROR, ValidationName.REQUIREMENT_VALIDATION, errorMessage, path));
-//            }
-//          }
-//        }
-//      }
-//    }
-//  }
-
   public void validate(TemplateSchemaArtifact templateSchemaArtifact, TemplateReporter templateReporter, TemplateInstanceValuesReporter valuesReporter, Consumer<ValidationResult> handler){
     //literate template schema and get all required fields
     var requiredFields = getAllRequiredFields(templateSchemaArtifact, templateReporter);
